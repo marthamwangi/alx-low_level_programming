7e7b478cf26d2467e10b2164d77dbfaadf0d61db
marthamwangi <martyturing@gmail.com>
2022-06-02 15:15:45 -0700

    Types on compilation 32 and 64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ int main(void)
 char _char;
 int _int;
 long int _long_int;
-long long int _long_long_int;
+long long int llint;
 float _float;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(_char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(_int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(_long_int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(_long_long_int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llint));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(_float));
 return (0);
 }
