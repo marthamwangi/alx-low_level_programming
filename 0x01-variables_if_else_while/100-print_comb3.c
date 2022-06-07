9a1215a664d442bb4156fd9660ac614dbc7b3840
marthamwangi <martyturing@gmail.com>
2022-06-07 07:13:42 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main -print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+	int tens;
+	int ones;
+
+	for (number = 0; number < 90; number++)
+	{
+		tens = (number / 10 + '0');
+		ones = (number % 10 + '0');
+	if (tens != ones)
+	{
+		putchar(tens);
+		putchar(ones);
+		if (number != 89)
+		{
+		putchar(',');
+		}
+	}
+	putchar(' ');
+	}
+	putchar('\n');
+return (0);
+}
