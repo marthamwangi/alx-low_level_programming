a7469c7a5e905b8fb064384ed9536740aeaa21f3
marthamwangi <martyturing@gmail.com>
2022-06-10 13:45:20 +0000

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * natural_numbers - Prints natural numbers below 1024 that are
  * multiplies of 3 or 5
- *
+ * @n: specify range
  * Return: Always 0.
  */
-int natural_numbers(int n){
+int natural_numbers(int n)
+{
 int i;
 int sum = 0;
-for( i = 3; i < n; i++)
-{    if((i % 3) == 0 || (i % 5) == 0)
-{
-sum = sum+i;
-}
 
+for (i = 3; i < n; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+sum = sum + i;
 }
-_putchar(sum + '0');
-return(0);
+}
+printf("%d\n", sum);
+return (0);
 }
